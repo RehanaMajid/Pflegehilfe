2cccac2956e507733492f57340af30ebc3c6cf24
rehanamajid <rehanamajid1993@gmail.com>
2023-03-26 18:07:01 +0200

    Code Refactoring

--- src/App.js
+++ src/App.js
@@ -1,137 +1,154 @@
 import './style/App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {solid, regular} from "@fortawesome/fontawesome-svg-core/import.macro";
-import {useState, useEffect} from "react";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {regular, solid} from "@fortawesome/fontawesome-svg-core/import.macro";
+import {useEffect, useState} from "react";
 
-const errorMessage='Error:Task name should be more than 10 characters including spaces.'
+const errorMessage = 'Error:Task name should be more than 10 characters including spaces.'
+
 function App() {
 
-  const [newTask, setNewTask] = useState({
-    title: "",
-    deadline: "",
-  })
+    const [newTask, setNewTask] = useState({
+        title: "",
+        deadline: "",
+    })
 
-  const [err, setErr] = useState(false)
+    const [taskError, setTaskError] = useState(false)
 
-  const [tasks, setTasks] = useState([])
+    const [deadLineError, setDeadLineError] = useState(false)
 
-  const [filter, setFilter] = useState("none")
+    const [tasks, setTasks] = useState([])
 
+    const [filter, setFilter] = useState("none")
 
-  function addTask(e){
-    e.preventDefault()
-    if(newTask.title.length <= 10){
-      setErr(true)
-    }else{
-      setTasks([...tasks, newTask])
-      setNewTask({title: ''})
-      localStorage.setItem('tasks', JSON.stringify([...tasks, newTask]))
+    console.log(deadLineError, newTask.deadline)
+
+    function addTask(e) {
+        e.preventDefault()
+        if (newTask.title.length <= 10) {
+            setTaskError(true)
+        } else if (newTask.deadline === "" || newTask.deadline === undefined) {
+            setDeadLineError(true)
+        } else {
+            setTasks([...tasks, newTask])
+            setNewTask({title: '', deadline: ''})
+            localStorage.setItem('tasks', JSON.stringify([...tasks, newTask]))
+        }
+
     }
 
-  }
+    function updateTask(key) {
 
-  function updateTask(key){
+        let update = tasks.slice();
 
-    let update = tasks.slice();
-
-    if(update[key].status === "active") {
-      update[key].status = "completed"
-    }else{
-      update[key].status = "active"
-    }
-    setTasks(update)
-    localStorage.setItem('tasks', JSON.stringify(tasks))
-  }
-
-  function deleteTask(key){
-    let update = tasks.slice()
-    update.splice(key, 1);
-    setTasks(update)
-    localStorage.setItem('tasks', JSON.stringify(update))
-  }
-
-  useEffect(()=>{
-    let LocalData = JSON.parse(localStorage.getItem('tasks'))
-
-    if(tasks.length === 0){
-      if(LocalData != null) {
-        setTasks(LocalData)
-      }
+        if (update[key].status === "active") {
+            update[key].status = "completed"
+        } else {
+            update[key].status = "active"
+        }
+        setTasks(update)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
     }
 
+    function deleteTask(key) {
+        let update = tasks.slice()
+        update.splice(key, 1);
+        setTasks(update)
+        localStorage.setItem('tasks', JSON.stringify(update))
+    }
 
-  }, [tasks])
+    useEffect(() => {
+        let LocalData = JSON.parse(localStorage.getItem('tasks'))
 
-  return (
-      <div className="App">
-        <header className="App-header">
-          <h2>Pflegehilfe app</h2>
-          {err ? <div className="error">{errorMessage}</div> : <></>}
-          <div className="todo-container">
-            <div className="input-container">
-              <FontAwesomeIcon icon={solid("angle-down")} className="icon" />
-              <form onSubmit={(e) => addTask(e)} className={"todo-form"}>
-                <input
-                    className="input-field"
-                    placeholder={"What needs to be done?"}
-                    value={newTask.title}
-                    onChange={(v) => {
-                      setNewTask({ ...newTask, title: v.currentTarget.value, status: "active"})
-                      setErr(false)
-                    }}
-                />
-                <input type="date" className="input-deadline" onChangeCapture={(v) => {
-                  setNewTask({...newTask, deadline: v.currentTarget.value})
-                  console.log(newTask)
-                }
-                } />
+        if (tasks.length === 0) {
+            if (LocalData != null) {
+                setTasks(LocalData)
+            }
+        }
 
-              </form>
-            </div>
-                <div className="todo-list">
-                  { tasks.filter(item => {
-                    if(filter !== "none") {
-                      return item.status === filter
-                    }else{
-                      return item
-                    }
-                  }).map((task, key) => {
-                    let overdue = false;
-                    if(new Date(task.deadline).toLocaleDateString() < new Date().toLocaleDateString()){
-                      overdue = true
-                    }
-                    return(
-                        <div className={"todo-item " + (overdue ? "overdue" : "")} key={key}>
-                          <div className="todo-inner">
-                            <FontAwesomeIcon
-                                icon={task.status === "active" ? regular("circle") : solid("circle")}
-                                onClick={() => updateTask(key)}
-                                className={"icon " + task.status}
+
+    }, [tasks])
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h2>Pflegehilfe app</h2>
+                {taskError ? <div className="error">{errorMessage}</div> : <></>}
+                {deadLineError ? <div className="error">Please select a deadline.</div> : <></>}
+                <div className="todo-container">
+                    <div className="input-container">
+                        <FontAwesomeIcon icon={solid("angle-down")} className="icon"/>
+                        <form onSubmit={(e) => addTask(e)} className={"todo-form"}>
+                            <input
+                                className="input-field"
+                                placeholder={"What needs to be done?"}
+                                value={newTask.title}
+                                onChange={(v) => {
+                                    setNewTask({...newTask, title: v.currentTarget.value, status: "active"})
+                                    setTaskError(false)
+                                }}
                             />
-                            <div className="todo-item-text">{task.title}</div>
-                          </div>
-                          <div className="todo-action">
-                            <div className="deadline">{task.deadline}</div>
-                            <div className="delete"><FontAwesomeIcon icon={solid("trash")} onClick={() => deleteTask(key)} /></div>
-                          </div>
+                            <input type="date" className="input-deadline"
+                                   value={newTask.deadline}
+                                   onChangeCapture={(v) => {
+                                       setNewTask({...newTask, deadline: v.currentTarget.value})
+                                       setDeadLineError(false)
+                                   }
+
+                                   }/>
+
+                        </form>
+                    </div>
+                    <div className="todo-list">
+                        {tasks.filter(item => {
+                            if (filter !== "none") {
+                                return item.status === filter
+                            } else {
+                                return item
+                            }
+                        }).map((task, key) => {
+                            let overdue = false;
+                            if (new Date(task.deadline).toLocaleDateString() < new Date().toLocaleDateString()) {
+                                overdue = true
+                            }
+                            return (
+                                <div className={"todo-item " + (overdue ? "overdue" : "")} key={key}>
+                                    <div className="todo-inner">
+                                        <FontAwesomeIcon
+                                            icon={task.status === "active" ? regular("circle") : solid("circle")}
+                                            onClick={() => updateTask(key)}
+                                            className={"icon " + task.status}
+                                        />
+                                        <div className="todo-item-text">{task.title}</div>
+                                    </div>
+                                    <div className="todo-action">
+                                        <div className="deadline">{task.deadline}</div>
+                                        <div className="delete"><FontAwesomeIcon icon={solid("trash")}
+                                                                                 onClick={() => deleteTask(key)}/></div>
+                                    </div>
+                                </div>
+                            )
+                        })}
+
+                    </div>
+
+                    <div className="todo-action-bar">
+                        <div className="number">{tasks.length} items left</div>
+                        <div className="filters">
+                            <div className={'filter ' + (filter === "none" ? "active" : "")}
+                                 onClick={() => setFilter("none")}>All
+                            </div>
+                            <div className={"filter " + (filter === 'active' ? "active" : "")}
+                                 onClick={() => setFilter("active")}>Active
+                            </div>
+                            <div className={"filter " + (filter === 'completed' ? "active" : "")}
+                                 onClick={() => setFilter("completed")}>Completed
+                            </div>
                         </div>
-                    )
-                  })}
-
+                    </div>
                 </div>
-
-            <div className="todo-action-bar">
-              <div className="number">{tasks.length} items left</div>
-              <div className="filters">
-                <div className={'filter ' + (filter === "none" ? "active" : "")} onClick={() => setFilter("none")}>All</div>
-                <div className={"filter " + (filter === 'active' ? "active" : "")} onClick={() => setFilter("active")}>Active</div>
-                <div className={"filter " + (filter === 'completed' ? "active" : "")} onClick={() => setFilter("completed")}>Completed</div>
-              </div>
-            </div>
-          </div>
-        </header>
-      </div>
-  );
+            </header>
+        </div>
+    );
 }
 
 export default App;
